1805bb9051f13a449eaf2b79671d550e3de0cdcb
m-chaabani <Arte2019>
2020-06-19 03:29:57 +0200

    ajouter functions et ses tests

---
+++ functions.py
@@ -0,0 +1,9 @@
+def get_full_name ( firstname, lastname):
+ """ return the full name in the format of firstname, lastname
+ 
+ Argument:
+ firstname: First Name e.g. Jhon
+ lastname: Last Name e.g. Doe
+ 
+ """
+ return firstname + ", " lastname
---
+++ test_functions.py
@@ -0,0 +1,2 @@
+def test_full_name(self):
+self.assertEqual("John , Doe", get_full_name("John","Doe"))
